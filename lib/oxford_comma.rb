2c021e80dcba78f7332c24d6b003f586f078385c
KJ Osenenko <karljosenenko@gmail.com>
2020-11-11 11:10:45 -0700

    commit tree 0e5282bdeedf840665fca3936dfd9ff0131c19dd

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    else
+        last = array.pop
+        final = []
+        final.push("and ", last)
+        array.push(final.join)
+        array.join(", ")
+    end
 end
